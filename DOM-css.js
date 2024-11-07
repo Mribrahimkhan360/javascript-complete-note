496f6aa5b27ae50ee35ebe3a5afdca884fc7ab1a
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2024-11-07 18:40:54 +0600

    Create DOM-css.js

---
+++ DOM-css.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('dom1');
+element.style.backgroundColor = 'red';
+element.style.height = '140px';
+element.style.color = 'white';
+element.style.display = 'flex';
+element.style.alignItems = 'center';
+element.style.textAlign = 'center';
