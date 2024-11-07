a39efb4bcfb7d083e3fcc94ba9a9cf1e37d70235
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2024-11-07 18:51:52 +0600

    Create setAttribute-getAttribute.js

---
+++ 39. DOM Manipulate CSS Class/setAttribute-getAttribute.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('dom1');
+
+let view = element.getAttribute('class');
+console.log(view);
+
+let dis = element.setAttribute('disable',true);
+console.log(dis);
